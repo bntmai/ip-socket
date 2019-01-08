6b66a34e3b9c13e090ad01d432c9c92372da9369
Mai Bui <maibui@Macbooks-MacBook-Pro-2.local>
2019-01-08 12:30:50 +0700

    Add routing

--- src/routes.js
+++ src/routes.js
@@ -5,11 +5,14 @@ const Dashboard = React.lazy(() => import('./views/Dashboard'));
 const Users = React.lazy(() => import('./views/SocialWeb/Users'));
 const UserDetail = React.lazy(() => import('./views/SocialWeb/UserDetail'));
 const CreateBlog = React.lazy(() => import('./views/SocialWeb/CreateBlog'));
-
+const Login = React.lazy(() => import('./views/SocialWeb/Login/Login'));
+const Register = React.lazy(() => import('./views/SocialWeb/Register/Register'));
 
 // https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
 const routes = [
   { path: '/', exact: true, name: 'Home', component: DefaultLayout },
+  { path: '/login', exact: true, name: 'Home', component: Login },
+  { path: '/register', exact: true, name: 'Home', component: Register },
   { path: '/dashboard', name: 'Dashboard', component: Dashboard },
   { path: '/users', exact: true,  name: 'Users', component: Users },
   { path: '/profile', exact: true, name: 'User Page', component: UserDetail },
