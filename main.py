f1b06af5784292739d93aaf9af98f9e7ed2ad326
ThanhDinhDat <datthanhlhp@gmail.com>
2019-01-09 09:26:40 +0700

    update login method

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from flask import Flask, jsonify, request, json
 import sqlite3
 # from datetime import datetime
 # from flask_cors import CORS
-# from flask_bcrypt import Bcrypt
-# from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from flask_jwt_extended import (create_access_token, create_refresh_token, jwt_required, jwt_refresh_token_required, get_jwt_identity, get_raw_jwt)
 
 app = Flask(__name__)
@@ -15,18 +15,17 @@ app = Flask(__name__)
 # app.config['JWT_SECRET_KEY'] = 'secret'
 #
 # mysql = MySQL(app)
-# bcrypt = Bcrypt(app)
-# jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 # CORS(app)
 
 @app.route('/api/auth/register/', methods=['POST'])
 def register():
     conn = sqlite3.connect('database.db')
-    print "somthing"
     cur = conn.cursor()
     email = request.get_json()['username']
-    password = request.get_json()['password']
+    password = bcrypt.generate_password_hash(request.get_json()['password']).decode('utf-8')
     dob = request.get_json()['dob']
     avatar = request.get_json()['avatar']
 
@@ -52,15 +51,18 @@ def login():
     conn = sqlite3.connect('database.db')
 
     cur = conn.cursor()
-    email = request.get_json()['email']
+    email = request.get_json()['username']
     password = request.get_json()['password']
     result = ""
-	
-    cur.execute("SELECT * FROM users where email = '" + str(email) + "'")
+
+    print email
+
+    cur.execute("SELECT * FROM user where email = '" + str(email) + "'")
     rv = cur.fetchone()
-	
-    if rv['password'] == password:
-        access_token = create_access_token(identity = {'email': rv['email'],'password': rv['password']})
+    print rv
+
+    if bcrypt.check_password_hash(rv['password'], password):
+        access_token = create_access_token(identity = {'first_name': rv['first_name'],'last_name': rv['last_name'],'email': rv['email']})
         result = access_token
     else:
         result = jsonify({"error":"Invalid username and password"})
