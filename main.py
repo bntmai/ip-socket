16c56449b173b034d0d8e075bdc302aeeb659a7b
Thinh Nguyen Tran Phuoc <ngtrphuocthinh@gmail.com>
2019-01-12 00:54:47 +0700

    update layout

--- main.py
+++ main.py
@@ -177,6 +177,7 @@ def get_user_by_id():
     conn.close()
     return jsonify({'result' : result}),201
 
+
 @app.route('/api/chat/sendMessages', methods=['POST'])
 def send_message():
     conn = sqlite3.connect('database.db')
@@ -198,7 +199,10 @@ def send_message():
             'content' : content,
         }
     conn.close()
-    return jsonify({}),201
+    print(result)
+    print("hfksjfhlkasjflasj")
+    return jsonify({}), 201
+
 
 @app.route('/api/chat/loadMessages/', methods=['POST'])
 def load_message():
@@ -206,22 +210,23 @@ def load_message():
     cur = conn.cursor()
     fromUserId = request.get_json()['fromUserId']
     toUserId = request.get_json()['toUserId']
-    content = request.get_json()['content']
-
+    print(fromUserId)
     cur.execute("SELECT * FROM conversation where (fromUserId = '" + str(fromUserId) + "' and toUserId = '" + str(toUserId) +
-    "') or (fromUserId = '" + str(toUserId) + "' and toUserId = '" + str(fromUserId) + "'")
+    "') or (fromUserId = '" + str(toUserId) + "' and toUserId = '" + str(fromUserId) + "' ORDER BY createdTime ASC")
     rv = cur.fetchall()
     result = []
     for mess in rv:
-        result.push({
+        result.append({
             'fromUserId': mess[1],
-            'toUserId' : mess[2],
-            'content' : mess[3],
+            'toUserId': mess[2],
+            'content': mess[3],
         })
+        print(result)
     conn.close()
-    return jsonify({'result' : result}),201
+    return jsonify({'result': result}), 200
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    socketio.run(app,debug=True)
+    socketio.run(app, debug=True)
+
--- src/views/SocialWeb/ChatWindow.js
+++ src/views/SocialWeb/ChatWindow.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { Badge, Table, Row, Label, Button } from 'reactstrap';
-import { chat } from "../../actions";
+
 import { Link } from 'react-router-dom';
-import Users from './Users'
-import { connect } from "react-redux";
+
+
+function UserRow(props) {
+    const user = props.user
+
+    const userLink = `/chat/users/${user.id}`
+
+    return (
+        <tr key={user.id.toString()}>
+            <th scope="row"><Link to={userLink}>{user.id}</Link></th>
+            <td><Link to={userLink}>{user.email}</Link></td>
+        </tr>
+    )
+}
+
+
 
 class ChatApp extends Component {
-
     constructor() {
 
         super()
@@ -21,10 +34,34 @@ class ChatApp extends Component {
             }],
             chatEmail: localStorage.getItem('chatEmail')
         }
+        this.onButtonClick.bind(this)
     }
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.state.chatEmail != nextState.chatEmail;
-      }
+    onButtonClick(e) {
+        e.stopPropagation()
+        e.preventDefault()
+        this.setState({
+            chatEmail: e.currentTarget.innerHTML
+        }) 
+        localStorage.setItem("chatEmail", e.currentTarget.innerHTML)
+        let currentEmail = localStorage.getItem("email")
+        let chatEmail = localStorage.getItem("chatEmail")
+        let headers = { "Content-Type": "application/json" };
+        let { token } = localStorage.getItem("access_token");
+
+        if (token) {
+            headers["Authorization"] = `Token ${token}`;
+        }
+        const userId = localStorage.getItem("id");
+        let body = JSON.stringify({ "fromUserId": currentEmail, "toUserId": chatEmail, "content": "hfdhd" });
+        fetch('http://127.0.0.1:5000/api/chat/loadMessages', { headers, method: "POST", body }).then(result => {
+            console.log(result.json())
+            return result.json();
+        }).then(data => {
+            this.setState({
+                message: ''
+            })
+        })
+    }
     render() {
         let userList = localStorage.getItem("userList")
         userList = JSON.parse(userList)
@@ -33,23 +70,24 @@ class ChatApp extends Component {
         return (
             <div className="app">
                 <div>
-
-                <div className="center-label"><Label>{this.state.chatEmail}</Label></div>
+                    <div className="center-label"><Label>{this.state.chatEmail}</Label></div>
                     <MessageList messages={this.state.messages} />
                     <SendMessageForm />
                 </div>
-                <ul>
-                    {userList.map((user) =>
-                      <Button block color="light" className="btn-square">{user.email}</Button> 
-                    )}
-                  </ul>
+                
+                    <ul>
+                        {userList.map((user, index) =>
+                            <button onClick={this.onButtonClick.bind(this)}>{user.email}</button>
+
+                        )}
+                    </ul>
             </div>
         )
     }
 }
 
 class MessageList extends Component {
-    render() {  
+    render() {
         return (
             <ul className="message-list">
                 {this.props.messages.map(message => {
@@ -96,8 +134,7 @@ class SendMessageForm extends Component {
         if (token) {
             headers["Authorization"] = `Token ${token}`;
         }
-        const userId = localStorage.getItem("id");
-        let body = JSON.stringify({ "fromUserId": currentEmail, "toUserId": chatEmail, "content":  this.state.message });
+        let body = JSON.stringify({ "fromUserId": currentEmail, "toUserId": chatEmail, "content": this.state.message });
         fetch('http://127.0.0.1:5000/api/chat/sendMessages', { headers, method: "POST", body }).then(result => {
             return result.json();
         }).then(data => {
